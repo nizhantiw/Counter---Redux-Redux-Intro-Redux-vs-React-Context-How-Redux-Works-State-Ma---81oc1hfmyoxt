0e27cf607567e4e84abe7a9e75f8640615c36c0d
nizhantiw <nizhantiw@gmail.com>
2023-01-12 12:42:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
-const App = () => {
+import React, { Component } from "react";
+import Counter from "./counterContainer";
 
-  const initialState = 0;
-const [state,dispatch] = useReducer(counterReducer, initialState)
-  return (
-    <div id="main">
-      <span id='counter'>{state}</span>
-      <div>
-        <button id='increment-btn' onClick={() => dispatch({type: "INCREMENT"})}>In</button>
-        <button id='decrement-btn' onClick={() => dispatch({type: "DECREMENT"})}>Dec</button>
-      </div>
-
-
-    </div>
-  )
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Counter />
+                </header>
+            </div>
+        );
+    }
 }
-
-
 export default App;
